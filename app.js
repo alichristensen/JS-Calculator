4b0c92b9ca5cdfa6e5cddd59d871b3774c68c60b
alichristensen <ali.christensen1@gmail.com>
2016-07-01 13:44:00 -0500

    calculator works but has major bugs

--- app.js
+++ app.js
@@ -1,40 +1,125 @@
-var numBtns = [];
-var opBtns = [];
+var inputDis = document.querySelector("input");
+var val1; 
+var val2;
+var result;
 
-var display = document.querySelector("#displayBox");
+var one = document.getElementById("btn1");
+var two = document.getElementById("btn2");
+var three = document.getElementById("btn3");
+var four = document.getElementById("btn4");
+var five = document.getElementById("btn5");
+var six = document.getElementById("btn6");
+var seven = document.getElementById("btn7");
+var eight = document.getElementById("btn8");
+var nine = document.getElementById("btn9");
+var zero = document.getElementById("btn0");
+var clear = document.getElementById("clear");
 
-numBtns[0] = document.getElementById("btn1");
-numBtns[1] = document.getElementById("btn2");
-numBtns[2] = document.getElementById("btn3");
-numBtns[3] = document.getElementById("btn4");
-numBtns[4] = document.getElementById("btn5");
-numBtns[5] = document.getElementById("btn6");
-numBtns[6] = document.getElementById("btn7");
-numBtns[7] = document.getElementById("btn8");
-numBtns[8] = document.getElementById("btn9");
-numBtns[9] = document.getElementById("btn0");
+var plus = document.getElementById("add");
+var minus = document.getElementById("sbtr");
+var times = document.getElementById("mult");
+var divsn = document.getElementById("divsn");
+var equals = document.getElementById("equals");
 
-opBtns[0] = document.getElementById("add");
-opBtns[1] = document.getElementById("sbtr");
-opBtns[2] = document.getElementById("mult");
-opBtns[3] = document.getElementById("divsn");
-opBtns[4] = document.getElementById("equals");
+//update display to show clicked buttons
+var dis = ""; 
 
-var fin; 
-var sin;
+one.addEventListener("click", function(){
+	dis = dis + "1";
+	inputDis.value = dis;
+});
+two.addEventListener("click", function(){
+	dis = dis + "2";
+	inputDis.value = dis;
+});
+three.addEventListener("click", function(){
+	dis = dis + "3";
+	inputDis.value = dis;
+});
+four.addEventListener("click", function(){
+	dis = dis + "4";
+	inputDis.value = dis;
+});
+five.addEventListener("click", function(){
+	dis = dis + "5";
+	inputDis.value = dis;
+});
+six.addEventListener("click", function(){
+	dis = dis + "6";
+	inputDis.value = dis;
+});
+seven.addEventListener("click", function(){
+	dis = dis + "7";
+	inputDis.value = dis;
+});
+eight.addEventListener("click", function(){
+	dis = dis + "8";
+	inputDis.value = dis;
+});
+nine.addEventListener("click", function(){
+	dis = dis + "9";
+	inputDis.value = dis;
+});
+zero.addEventListener("click", function(){
+	dis = dis + "0";
+	inputDis.value = dis;
+});
+clear.addEventListener("click", function(){
+	dis = "";
+	val1 = 0;
+	val2 = 0;
+	result = 0;
+	inputDis.value = dis;
+})
 
+plus.addEventListener("click", function(){
+	val1 = parseInt(dis, 10);
+	dis = "";
+	inputDis.value = 0;
 
-for (var a=0; a<numBtns.length; a++){
-	numBtns[a].addEventListener("click", function(){
-		var fin = a;
-		display.textContent = fin;
-	})
-}
+	equals.addEventListener("click", function(){
+	val2 = parseInt(dis, 10);
+	result = val1 + val2;
+	inputDis.value = result;
+	val1 = result;
+	});
+});
 
+minus.addEventListener("click", function(){
+	val1 = parseInt(dis, 10);
+	dis = "";
+	inputDis.value = 0;
 
-for (var b=0; b<opBtns.length; b++){
-	opBtns[b].addEventListener("click", function(){
-		console.log("op btn clicked");
-	})
-}
+	equals.addEventListener("click", function(){
+	val2 = parseInt(dis, 10);
+	result = val1 - val2;
+	inputDis.value = result;
+	val1 = result;
+	});
+});
 
+times.addEventListener("click", function(){
+	val1 = parseInt(dis, 10);
+	dis = "";
+	inputDis.value = 0;
+
+	equals.addEventListener("click", function(){
+	val2 = parseInt(dis, 10);
+	result = val1 * val2;
+	inputDis.value = result;
+	val1 = result;
+	});
+});
+
+divsn.addEventListener("click", function(){
+	val1 = parseInt(dis, 10);
+	dis = "";
+	inputDis.value = 0;
+
+	equals.addEventListener("click", function(){
+	val2 = parseInt(dis, 10);
+	result = val1/val2;
+	inputDis.value = result;
+	val1 = result;
+	});
+});
